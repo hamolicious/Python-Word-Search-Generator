45243769ed183e540dbae721b983c4b85e53f64a
hamolicious <kickfliptv8@gmail.com>
2022-08-14 12:43:37 +0100

    cleaned up and refactored

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 from random import choice, randint, shuffle
 import string
 import argparse
-from sys import argv
 import os
-from typing import Any
-
 
 def generate_grid(w: int, h: int) -> list[list[str]]:
 		grid = []
@@ -18,75 +15,136 @@ def generate_grid(w: int, h: int) -> list[list[str]]:
 		return grid
 
 
-def populate_grid(words: list[str], grid: list[list[str]]) -> list[list[str]]:
-		for word in words:
-				done = False
-				tries = 10
-				while not done:
-						try:
-								start_x = randint(0, len(grid[0]) - len(word))
-								start_y = randint(0, len(grid) - len(word))
-								vel = choice([(1, 0), (0, 1), (1, 1)])
-						except ValueError:
-								done = True
-								break
+def generate_vel() -> list[int]:
+	return choice(
+		[
+			[0, 1],
+			[1, 0],
+		]
+	)
 
-						valid_spot = True
-						x, y = start_x, start_y
-						for i in range(len(word)):
-								if grid[y][x] == ' ' or grid[y][x] == word[i]:
-										pass
-								else:
-										valid_spot = False
-										tries -= 1
-										break
 
-								x += vel[0]
-								y += vel[1]
+def generate_pos(w: int, h: int) -> list[int]:
+	return [
+		randint(0, w),
+		randint(0, h)
+	]
 
-						if tries <= 0:
-								done = True
 
-						if valid_spot:
-								x, y = start_x, start_y
-								for i in range(len(word)):
-										grid[y][x] = word[i].upper()
-										x += vel[0]
-										y += vel[1]
-								done = True
+def add_vec(vec1: list[int], vec2: list[int]) -> list[int]:
+	return [
+		vec1[0] + vec2[0],
+		vec1[1] + vec2[1],
+	]
 
-		alphabet = string.ascii_uppercase
-		for i in range(len(grid)):
-				for j in range(len(grid[0])):
-						if grid[i][j] == ' ':
-								grid[i][j] = choice(alphabet)
 
-		return grid
+def mult_vec(vec1: list[int], n: int) -> list[int]:
+	return [
+		vec1[0] * n,
+		vec1[1] * n
+	]
+
+
+def generate_mask_from_grid(grid: list[list[str]]) -> list[list[int]]:
+	mask = []
+
+	for row in grid:
+		mask_row = []
+		for _ in row:
+			mask_row.append(0)
+		mask.append(mask_row)
+
+	return mask
+
+
+def populate_grid_with_random_let(grid: list[list[str]]) -> list[list[str]]:
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			if grid[i][j] == ' ':
+				grid[i][j] = choice(string.ascii_uppercase)
+
+	return grid
+
+
+def populate_grid_with_words(words: list[str], grid: list[list[str]]) -> list[list[str]]:
+	# HACK: need to think of a better algo to generate the words
+	mask = generate_mask_from_grid(grid)
+	shuffle(words)
+	width, height = len(grid), len(grid[0])
+	attempts = 1000
+	added_words = []
+
+	for word in words:
+		valid = True
+		for _ in range(attempts):
+			pos = generate_pos(width, height)
+			vel = generate_vel()
+
+			end_point = add_vec(pos, mult_vec(vel, len(word)))
+			if end_point[0] < 0 or end_point[0] >= width\
+				or end_point[1] < 0 or end_point[1] >= height:
+				valid = False
+
+			if not valid:
+				continue
+
+			temp_pos = pos.copy()
+			for l in word:
+				mask_char = mask[temp_pos[1]][temp_pos[0]]
+				if not (mask_char == 0 or mask_char == l):
+					valid = False
+					break
+				temp_pos = add_vec(temp_pos, vel)
+
+			if not valid:
+				continue
+
+			for l in word:
+				grid[pos[1]][pos[0]] = l
+				mask[pos[1]][pos[0]] = l
+
+				pos = add_vec(pos, vel)
+
+			added_words.append(word)
+			break
+
+	if len(added_words) != len(words):
+		return populate_grid_with_words(words, generate_grid(width, height))
+
+	return grid
+
+
+def str_grid(grid: list[list[str]]) -> list[list[str]]:
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			grid[i][j] = str(grid[i][j])
+	return grid
 
 
 def draw_grid(grid: list[list[str]], used_words: list[str]) -> None:
-		used_words = iter(used_words)
-		pipe = '|'
+	grid = str_grid(grid)
+	used_words = iter(used_words)
+	pipe = '|'
 
-		screen = f'{pipe}{pipe.join(grid[0])}{pipe}'
-		line_len = len(screen.split('\n')[-1])
-		screen = ''
-		print(f'{" " * line_len}\tWords to Find:')
+	screen = f'{pipe}{pipe.join(grid[0])}{pipe}'
+	line_len = len(screen.split('\n')[-1])
+	screen = ''
+	print(f'{" " * line_len}\tWords to Find:')
 
-		for row in grid:
-			try:
-				word = next(used_words)
-			except StopIteration:
-				word = ''
-			screen += f'\n{pipe}{pipe.join(row)}{pipe}\t{word}'
+	for row in grid:
+		try:
+			word = next(used_words)
+		except StopIteration:
+			word = ''
+		screen += f'\n{pipe}{pipe.join(row)}{pipe}\t{word}'
 
-		print(screen)
-		while True:
-			try:
-				word = next(used_words)
-				print(f'{" " * line_len}\t{word}')
-			except StopIteration:
-				break
+	print(screen)
+	while True:
+		try:
+			word = next(used_words)
+			print(f'{" " * line_len}\t{word}')
+		except StopIteration:
+			break
 
 
 def use_random(path: str, num_words: int) -> list[str]:
@@ -98,7 +156,7 @@ def use_random(path: str, num_words: int) -> list[str]:
 
 			for _ in range(num_words):
 				shuffle(words)
-				output.append(words[0])
+				output.append(words[0].upper())
 				words.remove(words[0])
 
 				if len(words) == 0:
@@ -173,7 +231,8 @@ num_words = get_num_of_words()
 
 grid = generate_grid(width, height)
 words = use_random(path_word_list, num_words)
-grid = populate_grid(words, grid)
+grid = populate_grid_with_words(words, grid)
+grid = populate_grid_with_random_let(grid)
 
 clear()
 draw_grid(grid, words)
